57eb4bb308769b46fd42d0a9813d534654024b71
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-04-22 13:30:49 -0400

    Create health controller.

---
+++ Nexus.Discord.Forum.List.Server/Controller/HealthController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Microsoft.AspNetCore.Mvc;
+using Nexus.Discord.Forum.List.Server.Discord;
+
+namespace Nexus.Discord.Forum.List.Server.Controller;
+
+public class HealthController
+{
+    [HttpGet]
+    [Route("health")]
+    public ObjectResult HealthCheck()
+    {
+        if (Bot.GetBot().Client.ConnectionState == ConnectionState.Connected)
+        {
+            return new ObjectResult("UP");
+        }
+        else
+        {
+            return new ObjectResult("DOWN")
+            {
+                StatusCode = 503,
+            };
+        }
+    }
+}
