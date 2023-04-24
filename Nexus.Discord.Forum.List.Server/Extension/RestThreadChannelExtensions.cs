143a4f96bd6d05436fc6abc18da617a0fc559e05
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-04-23 20:35:26 -0400

    Add docstring.

--- Nexus.Discord.Forum.List.Server/Extension/RestThreadChannelExtensions.cs
+++ Nexus.Discord.Forum.List.Server/Extension/RestThreadChannelExtensions.cs
@@ -7,6 +7,11 @@ namespace Nexus.Discord.Forum.List.Server.Extension;
 
 public static class RestThreadChannelExtensions
 {
+    /// <summary>
+    /// Converts a RestThreadChannel to SocketThreadChannel.
+    /// </summary>
+    /// <param name="channel">REST channel to convert from.</param>
+    /// <returns>Converted socket thread channel.</returns>
     public static async Task<SocketThreadChannel> ToSocketThreadChannelAsync(this RestThreadChannel channel)
     {
         return (SocketThreadChannel) await Bot.GetBot().Client.GetChannelAsync(channel.Id);
